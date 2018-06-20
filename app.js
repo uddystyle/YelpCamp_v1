661c96168264b69304a976c5b1d18c0b881cd098
Tomohisa Uchida <uddystyle@gmail.com>
2018-06-20 11:32:23 +0900

    Implemented navbar

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 
 var campgrounds = [
-  { name: 'Salmon Creek', image: 'https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b014439cf3c479a5eeb0_340.jpg' },
-  { name: 'Granite Hill', image: 'https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439cf3c479a5eeb0_340.jpg' },
+  { name: 'Salmon Creek', image: 'https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439cf4c47fafedbd_340.jpg' },
+  { name: 'Granite Hill', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f9c57ca1e5b5b0_340.jpg' },
   { name: "Mountain Goat's Rest", image: 'https://farm1.staticflickr.com/112/316612921_f23683ca9d.jpg' },
-  { name: 'Salmon Creek', image: 'https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b014439cf3c479a5eeb0_340.jpg' },
-  { name: 'Granite Hill', image: 'https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439cf3c479a5eeb0_340.jpg' },
+  { name: 'Salmon Creek', image: 'https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439cf4c47fafedbd_340.jpg' },
+  { name: 'Granite Hill', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f9c57ca1e5b5b0_340.jpg' },
   { name: "Mountain Goat's Rest", image: 'https://farm1.staticflickr.com/112/316612921_f23683ca9d.jpg' },
-  { name: 'Salmon Creek', image: 'https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b014439cf3c479a5eeb0_340.jpg' },
-  { name: 'Granite Hill', image: 'https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439cf3c479a5eeb0_340.jpg' },
+  { name: 'Salmon Creek', image: 'https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439cf4c47fafedbd_340.jpg' },
+  { name: 'Granite Hill', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f9c57ca1e5b5b0_340.jpg' },
   { name: "Mountain Goat's Rest", image: 'https://farm1.staticflickr.com/112/316612921_f23683ca9d.jpg' }
 ]
 
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 })
 
 app.get('/campgrounds', function (req, res) {
-  res.render('campgrounds', {campgrounds: campgrounds});
+  res.render('campgrounds', {campgrounds: campgrounds})
 })
 
 app.post('/campgrounds', function (req, res) {
@@ -40,5 +40,5 @@ app.get('/campgrounds/new', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('The YelpCamp Server Has Started!!');
+  console.log('The YelpCamp Server Has Started!!')
 })
